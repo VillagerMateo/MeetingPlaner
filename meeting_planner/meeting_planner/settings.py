d434be69a3dd043a73e53ed60d373133a52a229d
Mateusz Płachta <plachta1998@gmail.com>
2022-08-17 22:26:05 +0200

    Add name link

--- meeting_planner/meeting_planner/settings.py
+++ meeting_planner/meeting_planner/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#$nna$z2%(k$*tf@a^s1fn^x#e9d+aq2$gzj)syn7y5g5%mo%t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'simplemeetingplanner.herokuapp.com']
 
 
 # Application definition
